16ed1add760438ff271a5385ab9daa1f17c39b19
natasha8775735@gmail.com <natalliakhadakouskaya>
2023-01-27 15:09:51 +0300

    HomeWork010

---
+++ HomeWork010/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("Введите число: ");
+int.TryParse(Console.ReadLine(), out int x);
+
+int FunctionCub(int x)
+{
+    return(int)Math.Pow(x, 3);
+}
+
+int i = 1;
+while(i <= x)
+{
+    Console.Write(FunctionCub(i));
+    if (i < x)
+        Console.Write(", ");
+        i++;
+}
